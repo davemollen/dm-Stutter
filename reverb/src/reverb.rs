60bd0c63061499b71d929b93504e6180ec318d04
dave <davemollen@gmail.com>
2023-02-27 16:31:15 +0100

    Make more readable

--- reverb/src/reverb.rs
+++ reverb/src/reverb.rs
@@ -29,6 +29,60 @@ impl Reverb {
     }
   }
 
+  fn map_reverb_parameters(
+    &mut self,
+    size: f32,
+    speed: f32,
+    depth: f32,
+    predelay: f32,
+    absorb: f32,
+    decay: f32,
+    tilt: f32,
+    mix: f32,
+  ) -> (f32, f32, f32, f32, f32, f32, f32, f32, f32) {
+    let predelay = self.smooth_predelay.run(predelay, 12., "hertz");
+    let size = self.smooth_size.run(size, 12., "hertz");
+    let depth = self
+      .smooth_depth
+      .run(depth.powf(4.) * MAX_DEPTH, 12., "hertz");
+    let absorb = self.smooth_absorb.run(absorb, 12., "hertz");
+    let tilt = self.smooth_tilt.run(tilt, 12., "hertz");
+    let decay = decay.powf(0.3333333);
+    let diffuse = (absorb * 3.).min(1.) * 0.8;
+    let absorb = ((absorb - 0.3333333).max(0.) * 1.5).powf(0.3333333);
+
+    (
+      size, speed, depth, predelay, absorb, diffuse, decay, tilt, mix,
+    )
+  }
+
+  fn get_predelay_output(&mut self, input: (f32, f32), predelay: f32) -> f32 {
+    let predelay_output = self.predelay_tap.read(predelay, "linear");
+    self.predelay_tap.write((input.0 + input.1) * FRAC_1_SQRT_2);
+    predelay_output
+  }
+
+  fn get_delay_taps_output(
+    &mut self,
+    input: f32,
+    size: f32,
+    speed: f32,
+    depth: f32,
+    diffuse: f32,
+    absorb: f32,
+    decay: f32,
+  ) -> (f32, f32) {
+    self
+      .taps
+      .run(input, size, speed, depth, diffuse, absorb, decay)
+  }
+
+  fn apply_tilt_filter(&mut self, input: (f32, f32), tilt: f32) -> (f32, f32) {
+    self
+      .tilt_filter
+      .run(input, 520., 6000., 3.981072, 15.848932, tilt)
+  }
+
   pub fn run(
     &mut self,
     input: (f32, f32),
@@ -41,25 +95,13 @@ impl Reverb {
     tilt: f32,
     mix: f32,
   ) -> (f32, f32) {
-    let predelay = self.smooth_predelay.run(predelay, 12., "hertz");
-    let size = self.smooth_size.run(size, 12., "hertz");
-    let depth = self
-      .smooth_depth
-      .run(depth.powf(4.) * MAX_DEPTH, 12., "hertz");
-    let absorb = self.smooth_absorb.run(absorb, 12., "hertz");
-    let tilt = self.smooth_tilt.run(tilt, 12., "hertz");
-    let decay = decay.powf(0.3333333);
-    let diffuse = (absorb * 3.).min(1.) * 0.8;
-    let absorb = ((absorb - 0.3333333).max(0.) * 1.5).powf(0.3333333);
+    let (size, speed, depth, predelay, absorb, diffuse, decay, tilt, mix) =
+      self.map_reverb_parameters(size, speed, depth, predelay, absorb, decay, tilt, mix);
 
-    let predelay_output = self.predelay_tap.read(predelay, "linear");
-    self.predelay_tap.write((input.0 + input.1) * FRAC_1_SQRT_2);
-    let taps_output = self
-      .taps
-      .run(predelay_output, size, speed, depth, diffuse, absorb, decay);
-    let reverb_output = self
-      .tilt_filter
-      .run(taps_output, 520., 6000., 3.981072, 15.848932, tilt);
-    Mix::run(input, reverb_output, mix)
+    let predelay_output = self.get_predelay_output(input, predelay);
+    let taps_output =
+      self.get_delay_taps_output(predelay_output, size, speed, depth, diffuse, absorb, decay);
+    let tilt_filter_output = self.apply_tilt_filter(taps_output, tilt);
+    Mix::run(input, tilt_filter_output, mix)
   }
 }
