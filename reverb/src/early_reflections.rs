b677314851805c680166a7ea6419f341636bf494
Dave Mollen <dave.mollen@infi.nl>
2023-05-30 22:07:18 +0200

    Simpler early reflections

--- reverb/src/early_reflections.rs
+++ reverb/src/early_reflections.rs
@@ -1,6 +1,6 @@
 use crate::{float_ext::FloatExt, tap::Tap, MAX_SIZE, MIN_SIZE};
 
-const EARLY_REFLECTIONS_INCREASE_IN_DB: f32 = -6.;
+const MINUS_TWELVE_DB: f32 = 0.251189;
 
 pub struct EarlyReflections {
   reflections_left: [f32; 6],
@@ -15,33 +15,20 @@ impl EarlyReflections {
     }
   }
 
-  fn read_early_reflection(
-    &self,
-    index: usize,
-    size: f32,
-    time_fraction: &f32,
-    tap: &mut Tap,
-  ) -> f32 {
-    let attenuation = if EARLY_REFLECTIONS_INCREASE_IN_DB > 0. {
-      (index as f32 / 6. * EARLY_REFLECTIONS_INCREASE_IN_DB - EARLY_REFLECTIONS_INCREASE_IN_DB)
-        .dbtoa()
-    } else {
-      (index as f32 / 6. * EARLY_REFLECTIONS_INCREASE_IN_DB).dbtoa()
-    };
-    tap.read_early_reflection(size, *time_fraction) * attenuation
+  fn read_early_reflection(&self, size: f32, time_fraction: &f32, tap: &mut Tap) -> f32 {
+    tap.read_early_reflection(size, *time_fraction)
   }
 
   fn process_channel(&mut self, reflections: [f32; 6], size: f32, tap: &mut Tap, gain: f32) -> f32 {
     reflections
       .iter()
-      .enumerate()
-      .map(|(index, time_fraction)| self.read_early_reflection(index, size, time_fraction, tap))
+      .map(|time_fraction| self.read_early_reflection(size, time_fraction, tap))
       .sum::<f32>()
       * gain
   }
 
   pub fn run(&mut self, size: f32, taps: &mut [Tap; 4]) -> (f32, f32) {
-    let gain = size.scale(MIN_SIZE, MAX_SIZE, 0., -12.).dbtoa();
+    let gain = size.scale(MIN_SIZE, MAX_SIZE, 1., MINUS_TWELVE_DB);
     let out_left = self.process_channel(self.reflections_left, size, &mut taps[0], gain);
     let out_right = self.process_channel(self.reflections_right, size, &mut taps[1], gain);
     (out_left, out_right)
