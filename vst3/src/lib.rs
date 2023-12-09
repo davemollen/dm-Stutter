c5597d5e3923433326b109d8d850195d6ee73da8
Dave Mollen <dave.mollen@infi.nl>
2023-12-09 14:13:18 +0100

    Revert renaming of in and output ports

--- vst3/src/lib.rs
+++ vst3/src/lib.rs
@@ -85,11 +85,13 @@ impl Plugin for DmReverb {
     let mix = self.params.mix.value();
 
     buffer.iter_samples().for_each(|mut channel_samples| {
-      let input_left = channel_samples.get_mut(0).unwrap();
-      let input_right = channel_samples.get_mut(1).unwrap();
+      let left_channel_in = channel_samples.get_mut(0).unwrap();
+      let input_left = *left_channel_in;
+      let right_channel_in = channel_samples.get_mut(1).unwrap();
+      let input_right = *right_channel_in;
 
       let (reverb_left, reverb_right) = self.reverb.run(
-        (*input_left, *input_right),
+        (input_left, input_right),
         reverse,
         predelay,
         size,
@@ -102,10 +104,10 @@ impl Plugin for DmReverb {
         mix,
       );
 
-      let output_left = channel_samples.get_mut(0).unwrap();
-      *output_left = reverb_left;
-      let output_right = channel_samples.get_mut(1).unwrap();
-      *output_right = reverb_right;
+      let left_channel_out = channel_samples.get_mut(0).unwrap();
+      *left_channel_out = reverb_left;
+      let right_channel_out = channel_samples.get_mut(1).unwrap();
+      *right_channel_out = reverb_right;
     });
     ProcessStatus::Normal
   }
