9502f51ecfd5c24cca82a68ef7c07ecd736460ed
davemollen <davemollen@gmail.com>
2025-06-09 13:31:51 +0200

    Add font to binary

--- nih-plug/src/editor.rs
+++ nih-plug/src/editor.rs
@@ -25,7 +25,7 @@ use nih_plug_vizia::vizia::{
   style::FontWeightKeyword,
   views::{Element, HStack, Label, VStack},
 };
-use nih_plug_vizia::{create_vizia_editor, ViziaState, ViziaTheming};
+use nih_plug_vizia::{create_vizia_editor, vizia_assets, ViziaState, ViziaTheming};
 use std::sync::Arc;
 use ui_data::{ParamChangeEvent, UiData};
 
@@ -46,7 +46,9 @@ pub(crate) fn create(
     editor_state,
     ViziaTheming::Custom,
     move |cx, gui_context| {
-      let _ = cx.add_stylesheet(STYLE);
+      vizia_assets::register_roboto(cx);
+      vizia_assets::register_roboto_bold(cx);
+      cx.add_stylesheet(STYLE).ok();
 
       UiData {
         params: params.clone(),
