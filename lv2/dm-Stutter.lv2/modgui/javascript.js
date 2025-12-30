456fe2749e563b738a837553a8478a1dd3226f41
Dave Mollen <davemollen@gmail.com>
2025-12-30 18:09:33 +0100

    Simplify mod gui

--- lv2/dm-Stutter.lv2/modgui/javascript.js
+++ lv2/dm-Stutter.lv2/modgui/javascript.js
@@ -1,58 +1,28 @@
 function(event) {
   function handle_event(symbol, value) {
     switch (symbol) {
-      case "on":
-        const on = event.icon.find("[mod-port-symbol=on]");
-        if(value == 1) {
-          on.addClass("on");
-        } else {
-          on.removeClass("on");
-        }
-        break;
       case "auto":
-        const auto = event.icon.find("[mod-port-symbol=auto]");
         const chance = event.icon.find("[mod-port-symbol=chance]");
         const duration = event.icon.find("[mod-port-symbol=duration]");
 
         if(value == 1) {
-          auto.addClass("on");
           chance.removeClass("disabled");
           duration.removeClass("disabled");
         } else {
-          auto.removeClass("on");
           chance.addClass("disabled");
           duration.addClass("disabled");
         }
         break;
-      case "trigger":
-        const trigger = event.icon.find("[mod-port-symbol=trigger]");
-        if(value == 1) {
-          trigger.addClass("on");
-        } else {
-          trigger.removeClass("on");
-        }
-        break;
       case "sync":
-        const sync = event.icon.find("[mod-port-symbol=sync]");
         const pulse = event.icon.find("[mod-port-symbol=pulse]");
         const tempo_factor = event.icon.find("[mod-port-symbol=tempo_factor]");
 
         if(value == 1) {
-          sync.addClass("on");
           pulse.addClass("hide");
           tempo_factor.removeClass("hide");
         } else {
-          sync.removeClass("on");
           pulse.removeClass("hide");
           tempo_factor.addClass("hide");
-        }
-        break;
-      case "dry_thru":
-        const dry_thru = event.icon.find("[mod-port-symbol=dry_thru]");
-        if(value == 1) {
-          dry_thru.addClass("on");
-        } else {
-          dry_thru.removeClass("on");
         }
         break;
       default:
